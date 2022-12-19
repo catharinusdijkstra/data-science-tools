c9937b8e2d0725a7944c9e148235f508ef9a6c9a
catharinusdijkstra <catharinusdykstra@gmail.com>
2022-12-19 22:04:02 +0100

    Update packages in setup.py.

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "black==22.3.0",
-        "matplotlib==3.5.2",
-        "pandas==1.2.4",
+        "black==22.12.0",
+        "matplotlib==3.6.2",
+        "pandas==1.5.2",
+        "pytest==7.2.0",
     ],
 )

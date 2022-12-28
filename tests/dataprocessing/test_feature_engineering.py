3758624d78d4501533a85658247fa2c5b7cce5e8
catharinusdijkstra <catharinusdykstra@gmail.com>
2022-12-28 15:13:15 +0100

    Include documentation to the function get_feature_lists.

--- tests/dataprocessing/test_feature_engineering.py
+++ tests/dataprocessing/test_feature_engineering.py
@@ -20,13 +20,13 @@ def test_get_feature_lists():
 
     features_expected = ["feature_1", "feature_2", "feature_3", "feature_4"]
     features_categorical_expected = ["feature_1"]
-    features_numeric_expected = ["feature_2", "feature_3"]
+    features_numerical_expected = ["feature_2", "feature_3"]
     features_boolean_expected = ["feature_4"]
 
     (
         features_actual,
         features_categorical_actual,
-        features_numeric_actual,
+        features_numerical_actual,
         features_boolean_actual,
     ) = get_feature_lists(column_data_types, keys, labels)
 
@@ -35,7 +35,7 @@ def test_get_feature_lists():
         features_categorical_expected, features_categorical_actual
     )
     unittest.TestCase().assertListEqual(
-        features_numeric_expected, features_numeric_actual
+        features_numerical_expected, features_numerical_actual
     )
     unittest.TestCase().assertListEqual(
         features_boolean_expected, features_boolean_actual
